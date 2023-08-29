864bfd17d5f86fc2fc962963ce8d52dd924365d5
Joan Abad <j.abad.reynoso@gmail.com>
2023-08-29 23:04:36 +0200

    Small camera touches to start at a good position with the camera

--- YorkieEngine/src/Camera/Camera.cpp
+++ YorkieEngine/src/Camera/Camera.cpp
@@ -2,7 +2,7 @@
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
 #include "Modules/CameraModule.h"
-
+#include "Graphics/Renderer.h"
 Camera::Camera(Viewport* viewport) : GameEntity(viewport),
 	cameraTarget(glm::vec3(0.f, 0.f, 0.f)),
 	cameraDirection(glm::vec3(0.f, 0.f, 0.f)),
@@ -10,12 +10,22 @@ Camera::Camera(Viewport* viewport) : GameEntity(viewport),
 	cameraUp(glm::vec3(0.0f, 1.0f, 0.0f)),
 	nearPlane(0.1f), farPlane(400.f), FOV(45.f),
 	projection(glm::mat4(1.0f)),
-	pitch(0), roll(0), yaw(-90)
+	pitch(-25), roll(0), yaw(-90)
 {
-	position = glm::vec3(0.0f, 0.0f, 25.0f);
+	position = glm::vec3(0.0f, 10.0f, 25.0f);
 	view = glm::lookAt(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 	this->entityName = "Camera";
 	cameraRight = glm::normalize(glm::cross(cameraUp, cameraDirection));
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	cameraFront = glm::normalize(front);
+	cameraRight = glm::normalize(glm::cross(front, glm::vec3(0.0f, 1.0f, 0.0f)));
+	cameraUp = glm::normalize(glm::cross(cameraRight, front));
+	
 }
 
 void Camera::SetViewMatrix(glm::mat4 & matrix)
